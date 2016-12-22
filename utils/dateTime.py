0bc299f4be5629e91f90668d8635cc929766f831
Swordk <wangyijian0525@gmail.com>
2016-12-22 14:36:06 +0800

    [MOD] dateTime.py: isValidDate_IsoExt / isValidDate_Iso / DateFromat_Iso2IsoExt / DateFormat_IsoExt2Iso

--- utils/dateTime.py
+++ utils/dateTime.py
@@ -5,8 +5,8 @@
 
 import time
 
-def is_valid_date(str):
-    '''判断是否是一个有效的日期字符串'''
+# YYYY-MM-DD
+def isValidDate_IsoExt(str):
     try:
         if (len(str) != 10):
             return False
@@ -15,13 +15,41 @@ def is_valid_date(str):
     except:
         return False
 
-# print(is_valid_date('2016-12-31'))
-# print(is_valid_date('2016-2-2'))
-# print(is_valid_date('2016-02-29'))
-# print(is_valid_date('20161231'))
-# print(is_valid_date('2016-12-32'))
-# print(is_valid_date('2015-02-29'))
+# YYYYMMDD
+def isValidDate_Iso(str):
+    '''判断是否是一个有效的日期字符串'''
+    try:
+        if (len(str) != 8):
+            return False
+        time.strptime(str, "%Y%m%d")
+        return True
+    except:
+        return False
 
-# a = "2013-02-2 23:40:00"
-# timeArray = time.strptime(a, "%Y-%m-%d %H:%M:%S")
-# print(time.strftime("%Y-%m-%d %H:%M:%S", timeArray))
+def DateFromat_Iso2IsoExt(strIso):
+    if (isValidDate_Iso(strIso) == False):
+        return None
+    timeArray = time.strptime(strIso, "%Y%m%d")
+    return time.strftime("%Y-%m-%d", timeArray)
+
+def DateFormat_IsoExt2Iso(strIsoExt):
+    if (isValidDate_IsoExt(strIsoExt) == False):
+        return None
+    timeArray = time.strptime(strIsoExt, "%Y-%m-%d")
+    return time.strftime("%Y%m%d", timeArray)
+
+# print(isValidDate_IsoExt('2016-12-31'))
+# print(isValidDate_IsoExt('2016-2-2'))
+# print(isValidDate_IsoExt('2016-02-29'))
+# print(isValidDate_IsoExt('20161231'))
+# print(isValidDate_IsoExt('2016-12-32'))
+# print(isValidDate_IsoExt('2015-02-29'))
+
+# print(isValidDate_Iso('20160202'))
+# print(isValidDate_Iso('20160229'))
+# print(isValidDate_Iso('20150229'))
+# print(isValidDate_Iso('2016-02-02'))
+# print(isValidDate_Iso('2016202'))
+
+# print(DateFromat_Iso2IsoExt('20100203'))
+# print(DateFormat_IsoExt2Iso('2010-02-03'))
