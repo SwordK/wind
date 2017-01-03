c4f7d8b078de1ea7c150dd57593b46e3e61ee226
Swordk <wangyijian0525@gmail.com>
2017-01-03 15:48:12 +0800

    [ADD] windDbBusiness.py, windStockSectionProcessor.py

---
+++ windDb/windDbBusiness.py
@@ -0,0 +1,174 @@
+﻿# -*- coding:utf-8 -*-
+# Create On 20161222
+# Auth: wang.yijian
+# desc: wind 数据库 业务处理
+
+import sys;sys.path.append("../")
+
+import datetime
+
+import business.tradingCalendar as tc
+import business.stockPool as stockPool
+import business.industry as industry
+import business.mdBar as mdBar
+import business.stockSection as stockSection
+import windDb
+
+# DBReqTradingCalendar {
+def DBReqTradingCalendar(strHost, strUser, strPwd, strDb):
+    dbInst = windDb.CWindDb(strHost, strUser, strPwd, strDb)
+    tcInst = tc.CTradingCalendar()
+
+    strExchange = 'SSE'
+    setRtn = dbInst.DBReqTradingCalendar(strExchange)
+    if (setRtn != None and len(setRtn) > 0):
+        tcInst.Add(strExchange, setRtn)
+
+    strExchange = 'SZSE'
+    setRtn = dbInst.DBReqTradingCalendar(strExchange)
+    if (setRtn != None and len(setRtn) > 0):
+        tcInst.Add(strExchange, setRtn)
+
+    strExchange = 'CFFEX'
+    setRtn = dbInst.DBReqTradingCalendar(strExchange)
+    if (setRtn != None and len(setRtn) > 0):
+        tcInst.Add(strExchange, setRtn)
+
+    strExchange = 'SHFE'
+    setRtn = dbInst.DBReqTradingCalendar(strExchange)
+    if (setRtn != None and len(setRtn) > 0):
+        tcInst.Add(strExchange, setRtn)
+
+    strExchange = 'DCE'
+    setRtn = dbInst.DBReqTradingCalendar(strExchange)
+    if (setRtn != None and len(setRtn) > 0):
+        tcInst.Add(strExchange, setRtn)
+
+    strExchange = 'CZCE'
+    setRtn = dbInst.DBReqTradingCalendar(strExchange)
+    if (setRtn != None and len(setRtn) > 0):
+        tcInst.Add(strExchange, setRtn)
+# } end of DBReqTradingCalendar
+
+
+# DbReqStockPool {
+def DBReqStockPool(strHost, strUser, strPwd, strDb, euSpt):
+    strWindCode = stockPool.GetStockPoolWindCode(euSpt)
+    if (strWindCode == 'None'):
+        return False
+    dbInst = windDb.CWindDb(strHost, strUser, strPwd, strDb)
+    listStockPool = dbInst.DBReqStockPool(strWindCode)
+
+    spManager = stockPool.CStockPoolManager()
+    spInst = stockPool.CStockPool(euSpt)
+    for rowElem in listStockPool:
+        if (len(rowElem) != 4):
+            continue
+        elem = stockPool.CStockPoolElem(rowElem[0], int(rowElem[1]), int(rowElem[2]), int(rowElem[3]))
+        spInst.AddElem(elem)
+
+    spInst.GenerateTdIndex()
+    if (False == spManager.SetStockPool(spInst)):
+        return False
+    return True
+# } end of DBReqStockPool
+
+# DBReqIndustries_ZX {
+def DBReqIndustries_ZX(strHost, strUser, strPwd, strDb):
+    dbInst = windDb.CWindDb(strHost, strUser, strPwd, strDb)
+    listStockPool = dbInst.DBReqIndustries_ZX()
+
+    ibiManager = industry.CIndustryBaseInfoManager()
+    for row in listStockPool:
+        if (len(row) != 6):
+            continue
+        industryInst = industry.CIndustryBaseInfo(row[0], row[1], row[4], int(row[2]), int(row[3]))
+        ibiManager.Add(industryInst)
+# } end of DBReqIndustries_ZX
+
+# DBReqStockIndustries_ZX {
+def DBReqStockIndustries_ZX(strHost, strUser, strPwd, strDb, strDateFrom = '', strDateTo = ''):
+    dbInst = windDb.CWindDb(strHost, strUser, strPwd, strDb)
+    listStockIndustries = dbInst.DBReqStockIndustries_ZX()
+
+    sipManager = industry.CStockIndustryPeriodManager()
+    for row in listStockIndustries:
+        if (len(row) != 5):
+            continue
+        sipManager.AddPeriod(row[0], row[1], row[2], row[3])
+
+    sipManager.GenerateTdIndex(strDateFrom, strDateTo)
+    # sipManager.Print____dictSipL1ByTd1()
+    # sipManager.Print__dictSipL1()
+    # print("******************************************")
+    # sipManager.Print__dictSipL1BySid()
+# } end of DBReqStockIndustries_ZX
+
+# DBReqStockEODPrice {
+def DBReqStockEODPrice(strHost, strUser, strPwd, strDb, listStocks, strDateFrom = '', strDateTo = ''):
+    dbInst = windDb.CWindDb(strHost, strUser, strPwd, strDb)
+    listStockEODPrice = dbInst.DBReqStockEODPrice(listStocks, strDateFrom, strDateTo)
+    mdbarMgr = mdBar.CMdBarDataManager()
+    for row in listStockEODPrice:
+        if (len(row) != 9):
+            continue
+        mdbarData = mdBar.CMdBarData(mdBar.EU_MdBarInterval.mdbi_1d, float(row[2]), float(row[3]), float(row[4]), float(row[5]), float(row[6]), float(row[7]), float(row[8]), int(row[1]))
+        mdbarMgr.Add(row[0], mdbarData)
+    return True
+# } end of DBReqStockEODPrice
+
+# DBReqStockSections {
+def DBReqStockSections(strHost, strUser, strPwd, strDb, euSst, listStocks, strDateFrom = '', strDateTo = ''):
+    dbInst = windDb.CWindDb(strHost, strUser, strPwd, strDb)
+    listStockSection = dbInst.DBReqStockSections(euSst, listStocks, strDateFrom, strDateTo)
+
+    ssMgr = stockSection.CStockSectionRecordsManager()
+    for row in listStockSection:
+        print(row)
+        if (euSst == stockSection.EU_StockSectionType.euSst_Evaluation or euSst == stockSection.EU_StockSectionType.euSst_MarketValue):
+            if (len(row) == 9):
+                dPe = None
+                dPb = None
+                dPcf = None
+                dPs = None
+                d_val_mv = None
+                d_dq_mv = None
+                d_freeshared_today = None
+                if (row[2] != None):
+                    dPe = float(row[2])
+                if (row[3] != None):
+                    dPb = float(row[3])
+                if (row[4] != None):
+                    dPcf = float(row[4])
+                if (row[5] != None):
+                    dPs = float(row[5])
+                if (row[6] != None):
+                    d_val_mv = float(row[6])
+                if (row[7] != None):
+                    d_dq_mv = float(row[7])
+                if (row[8] != None):
+                    d_freeshared_today = float(row[8])
+                ssMgr.AddValue1(row[0], int(row[1]), dPe, dPb, dPcf, dPs, d_val_mv, d_dq_mv, d_freeshared_today)
+        elif (euSst == stockSection.EU_StockSectionType.euSst_Growing or euSst == stockSection.EU_StockSectionType.euSst_Quality):
+            if (len(row) == 7):
+                d_qfa_yoynetprofit = None
+                d_qfa_yoysales = None
+                d_fa_yoy_equity = None
+                d_fa_yoyroe = None
+                d_fa_yoyocf = None
+                if (row[2] != None):
+                    d_qfa_yoynetprofit = float(row[2])
+                if (row[3] != None):
+                    d_qfa_yoysales = float(row[3])
+                if (row[4] != None):
+                    d_fa_yoy_equity = float(row[4])
+                if (row[5] != None):
+                    d_fa_yoyroe = float(row[5])
+                if (row[6] != None):
+                    d_fa_yoyocf = float(row[6])
+                ssMgr.AddValue2(row[0], int(row[1]), d_qfa_yoynetprofit, d_qfa_yoysales, d_fa_yoy_equity, d_fa_yoyroe, d_fa_yoyocf)
+    return True
+# } end of DBReqStockSections
+
+#listStocks = ['603883.SH' , '603885.SH' ,'603899.SH','603993.SH']
+# DBReqStockSections('10.63.6.100','ForOTC','otc12345678','WindDB', stockSection.EU_StockSectionType.euSst_Evaluation, listStocks, '20160101', '20161231')
---
+++ windDb/windStockSectionProcessor.py
@@ -0,0 +1,161 @@
+﻿# -*- coding:utf-8 -*-
+# Create On 20161221
+# Auth: wang.yijian
+# desc: wind 量化接口 数据处理
+
+import sys;sys.path.append("../")
+import getopt
+import business.tradingCalendar as tc
+import business.stockPool as stockPool
+import business.stockSection as stockSection
+import windDbBusiness
+
+
+# @return: dateinterval, groupsize, begindate, enddate, stockpool, stocksection, industrialneutral
+def ParseOpts(strAppName, argv):
+    strUsage = strAppName + ' -i <dateinterval> -g <groupsize> -b <begindate> -e <enddate> -p <stockpool> -s <stocksection> -n <neutral>\n'
+    strUsage += "\tDESCRIPTION \n"
+    strUsage += "\t\t-i, --dateinterval\n"
+    strUsage += "\t\t\t[int]    set Adjust Portfolios Date Interval\n"
+    strUsage += "\t\t-g, --groupsize\n"
+    strUsage += "\t\t\t[int]    set Stock Section Group Size\n"
+    strUsage += "\t\t-b, --begindate\n"
+    strUsage += "\t\t\t[date]   set Begin Date [yyyymmdd]\n"
+    strUsage += "\t\t-e, --enddate\n"
+    strUsage += "\t\t\t[date]   set End Date [yyyymmdd]\n"
+    strUsage += "\t\t-p, --stockpool\n"
+    strUsage += "\t\t\t[string] set Stock Pool. See More Input [-p More]\n"
+    strUsage += "\t\t-s, --stocksection\n"
+    strUsage += "\t\t\t[string] set Stock Section. See More Input [-s More]\n"
+    strUsage += "\t\t-n, --neutral\n"
+    strUsage += "\t\t\t[string] set Industrial neutral. Default is none. [none|common|ols]\n"
+    try:
+      opts, args = getopt.getopt(argv,"hi:g:b:e:p:s:n:",["dateinterval=","groupsize=","begindate=","enddate=","stockpool=","stocksection=","neutral="])
+    except getopt.GetoptError:
+        print('params error')
+        print(strUsage)
+        sys.exit(2)
+
+    if (len(opts) == 0):
+        print(strUsage)
+        sys.exit(2)
+
+    nDateInterval = 0
+    nGroupSize = 0
+    strBeginDate = ''
+    strEndDate = ''
+    strStockPool = ''
+    strStockSection = ''
+    strIndustrialNeutral = 'none'
+    for opt, arg in opts:
+        if opt == '-h':
+            print(strUsage)
+            sys.exit()
+        elif opt in ("-i", "--dateinterval"):
+            nDateInterval = int(arg)
+        elif opt in ("-g", "--groupsize"):
+            nGroupSize = int(arg)
+        elif opt in ("-b", "--begindate"):
+            strBeginDate = arg
+        elif opt in ("-e", "--enddate"):
+            strEndDate = arg
+        elif opt in ("-p", "--stockpool"):
+            strStockPool = arg
+        elif opt in ("-s", "--stocksection"):
+            strStockSection = arg
+        elif opt in ("-n", "--neutral"):
+            strIndustrialNeutral = arg
+
+    euSpt = stockPool.GetStockPoolByStr(strStockPool)
+    if (strStockPool == 'More' or (euSpt == stockPool.EU_StockPoolType.euspt_None and strStockPool != '')):
+        strSupportedSp = '-p ' + stockPool.GetSupportedSp()
+        print(strSupportedSp)
+        sys.exit(2)
+    euSs = stockSection.SsFromString(strStockSection)
+    if (strStockSection == 'More' or (euSs == stockSection.EU_StockSection.euSs_None and strStockSection != '')):
+        strSupportedSs = '-p ' + stockSection.GetSupportedSs()
+        print(strSupportedSs)
+        sys.exit(2)
+
+    if (nDateInterval <= 0):
+        print('-i <--dateinterval> is needed')
+        print(strUsage)
+        sys.exit(2)
+    if (nGroupSize <= 0):
+        print('-g <--groupsize> is needed')
+        print(strUsage)
+        sys.exit(2)
+    if (strBeginDate == '' or strEndDate == ''):
+        print('-b <--begindate> and -e <--enddate> is needed')
+        print(strUsage)
+        sys.exit(2)
+    if (euSpt == stockPool.EU_StockPoolType.euspt_None):
+        print('-p <--stockpool> is needed')
+        print(strUsage)
+        sys.exit(2)
+    if (euSs == stockSection.EU_StockSection.euSs_None):
+        print('-s <--stocksection> is needed')
+        print(strUsage)
+        sys.exit(2)
+    return nDateInterval, nGroupSize, strBeginDate, strEndDate, euSpt, euSs, strIndustrialNeutral
+
+def AdjustPosition(stockPoolInst, dtTradingDay, euSst, dFundTotal):
+    if (isinstance(stockPoolInst, stockPool.CStockPool) == False):
+        print('stockPoolInst is not instance of CStockPool')
+        return False
+
+
+
+
+def ProcessStockSections(strDbHost, strDbUserName, strDbPasswd, strDbDatabase, \
+    nDateInterval, nGroupSize, strBeginDate, strEndDate, euSpt, euSs, strIndustrialNeutral):
+
+    print('DBReqTradingCalendar ...')
+    windDbBusiness.DBReqTradingCalendar(strDbHost, strDbUserName, strDbPasswd, strDbDatabase)
+    tcInst = tc.CTradingCalendar()
+
+    print('DBReqIndustries_ZX ...')
+    windDbBusiness.DBReqIndustries_ZX(strDbHost, strDbUserName, strDbPasswd, strDbDatabase)
+
+    print('DBReqStockPool ...')
+    if (False == windDbBusiness.DBReqStockPool(strDbHost, strDbUserName, strDbPasswd, strDbDatabase, euSpt)):
+        print('DBReqStockPool Failed ...')
+        sys.exit(3)
+
+    spManager = stockPool.CStockPoolManager()
+    spInst = spManager.GetStockPool(euSpt)
+    setStocks = spInst.GetStocksByDatePeriod(strBeginDate, strEndDate)
+
+    print('DBReqStockEODPrice ...')
+    windDbBusiness.DBReqStockEODPrice(strDbHost, strDbUserName, strDbPasswd, strDbDatabase, setStocks, strBeginDate, strEndDate)
+
+    print('DBReqStockSections ...')
+    euSst = stockSection.Ss2Sst(euSs)
+    windDbBusiness.DBReqStockSections(strDbHost, strDbUserName, strDbPasswd, strDbDatabase, euSst, setStocks, strBeginDate, strEndDate)
+
+    pass
+
+
+
+def main(listArgs):
+    listParams = ParseOpts(listArgs[0], listArgs[1:])
+    if (len(listParams) != 7):
+        print('params parse error')
+        sys.exit(2)
+    strDbHost = ''
+    strDbUserName = ''
+    strDbPasswd = ''
+    strDbDatabase = ''
+
+    strDbHost = '10.63.6.100'
+    strDbUserName = 'ForOTC'
+    strDbPasswd = 'otc12345678'
+    strDbDatabase = 'WindDB'
+
+    ProcessStockSections(strDbHost, strDbUserName, strDbPasswd, strDbDatabase, \
+        listParams[0], listParams[1], listParams[2], listParams[3], listParams[4], listParams[5], listParams[6])
+
+if __name__ == '__main__':
+    main(sys.argv)
+
+
